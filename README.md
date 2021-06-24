52c15fedd199427c30be2a335556ebbf5c0cc441
anastasiya.krivokora <anastasiya.krivokora@quadcode.com>
2021-06-24 09:44:38 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# qa_guru_5_24_final_project
+# Автотесты на http://github.com :woman_technologist:
+
+## Реализованы проверки:
+в реализации использованы разные подходы проектирования, позволяющие достичь разного представления в отчетах
+### UI
+- [X] - 1
+- [X] - 2
+- [X] - 3
+- [X] - 4
+### API
+- [X] - 1
+- [X] - 2
+- [X] - 3
+- [X] - 4
+<a href="https://www.jetbrains.com/idea/">
+    <img src="https://starchenkov.pro/qa-guru/img/skills/Intelij_IDEA.svg" width="40" height="40"  alt="IDEA"/>
+</a>
+<a href="https://www.jetbrains.com/idea/">
+    <img src="https://starchenkov.pro/qa-guru/img/skills/Java.svg" width="40" height="40"  alt="Java"/>
+</a>
+<a href="https://www.jetbrains.com/idea/">
+    <img src="https://starchenkov.pro/qa-guru/img/skills/Selenide.svg" width="40" height="40"  alt="Seleide"/>
+</a>
+<a href="https://www.jetbrains.com/idea/">
+    <img src="https://starchenkov.pro/qa-guru/img/skills/Selenoid.svg" width="40" height="40"  alt="Selenoid"/>
+</a>
+<a href="https://www.jetbrains.com/idea/">
+    <img src="https://starchenkov.pro/qa-guru/img/skills/Gradle.svg" width="40" height="40"  alt="Gradle"/>
+</a>
+<a href="https://www.jetbrains.com/idea/">
+    <img src="https://starchenkov.pro/qa-guru/img/skills/JUnit5.svg" width="40" height="40"  alt="JUnit 5"/>
+</a>
+<a href="https://www.jetbrains.com/idea/">
+    <img src="https://starchenkov.pro/qa-guru/img/skills/Allure_Report.svg" width="40" height="40"  alt="Allure SE"/>
+</a>
+<a href="https://www.jetbrains.com/idea/">
+    <img src="https://starchenkov.pro/qa-guru/img/skills/Allure_EE.svg" width="40" height="40"  alt="Allure EE"/>
+</a>
+<a href="https://www.jetbrains.com/idea/">
+    <img src="https://starchenkov.pro/qa-guru/img/skills/Github.svg" width="40" height="40"  alt="Github"/>
+</a>
+<a href="https://www.jetbrains.com/idea/">
+    <img src="https://starchenkov.pro/qa-guru/img/skills/Jenkins.svg" width="40" height="40"  alt="Jenkins"/>
+</a>
+<a href="https://www.jetbrains.com/idea/">
+    <img src="https://starchenkov.pro/qa-guru/img/skills/Rest-Assured.svg" width="40" height="40"  alt="ResrAssured"/>
+</a>
+<a href="https://www.jetbrains.com/idea/">
+    <img src="https://starchenkov.pro/qa-guru/img/skills/Telegram.svg" width="40" height="40"  alt="Telegram"/>
+</a>
+<a href="https://www.jetbrains.com/idea/">
+    <img src="https://starchenkov.pro/qa-guru/img/skills/Jira.svg" width="40" height="40"  alt="Jira"/>
+</a>
+
+IntelliJ IDEA, Java, Selenide, Selenoid, Gradle, JUnit5, Allure SE, Allure TestOps, Github, Jenkins, Rest-Assured, Telegram (reports), Jira.
+
+
+### Команда для запуска из терминала
+
+### Видео о прохождении тестов
+
+### Запуск в Jenkins
+
+### Запуск на Selenoid
+
+### Отчёт в Allure Report
+![Allure](src/test/resources/files/Allure.png)
+
+### Хранение тест-кейсов в Allure TestOps
+![Allure](src/test/resources/files/Allure.png)
+
+### Интерграция с Jira
+![Jira](src/test/resources/files/Jira.png)
+
+### Уведомления в Telegram
+![Telegram](src/test/resources/files/Telegram_notify.png)
---
+++ src/test/java/tests/ui/GitHubUITests.java
@@ -0,0 +1,113 @@
+package tests.ui;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import customAnnotations.AutoMember;
+import customAnnotations.Component;
+import customAnnotations.JiraIssue;
+import customAnnotations.JiraIssues;
+import customAnnotations.Layer;
+import customAnnotations.ManualMember;
+import io.qameta.allure.AllureId;
+import io.qameta.allure.Feature;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+import tests.ui.pages.GitHubPage;
+import tests.BaseTest;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static org.openqa.selenium.By.id;
+
+@Layer("web")
+public class GitHubUITests extends BaseTest {
+
+    public final static String BASE_URL = "https://github.com";
+    private final static String REPOSITORY = "nastiykr/qa_guru_5_5_allure_reports";
+    private final static String TAB = "Issue";
+
+    GitHubPage gitHubPage = new GitHubPage();
+
+    @Test
+    @JiraIssues({@JiraIssue("QC5-5")})
+    @AllureId("3245")
+    @ManualMember("c05-nasstasya")
+    @AutoMember("nastiykr")
+    @Component("UI")
+    @Feature("Поиск")
+    @Tags({@Tag("critical"), @Tag("web")})
+    @DisplayName("Поиск репозитория через поисковую строку")
+      void searchRepo() {
+        gitHubPage.openPage(BASE_URL)
+                .search(REPOSITORY);
+    }
+
+    @Test
+    @JiraIssue("QC5-5")
+    @AllureId("3235")
+    @ManualMember("c05-nasstasya")
+    @AutoMember("nastiykr")
+    @Component("UI")
+    @Feature("Поиск")
+    @Tags({@Tag("web")})
+    @DisplayName("Поиск раздела Issue в репозитории")
+    void searchIssueTabTest() {
+        gitHubPage.openPage(BASE_URL)
+                .search(REPOSITORY)
+                .goToRepository(REPOSITORY)
+                .checkData(TAB);
+    }
+
+    @Test
+    @JiraIssue("QC5-5")
+    @AllureId("3236")
+    @ManualMember("c05-nasstasya")
+    @AutoMember("nastiykr")
+    @Component("UI")
+    @Feature("Авторизация")
+    @Tags({@Tag("web")})
+    @DisplayName("Авторизация пользователя с неверным паролем")
+    void authorizationUserWithNotRightPassword() {
+        gitHubPage.openPage(BASE_URL);
+        $(byText("Sign in")).click();
+        $(id("login_field")).setValue("user");
+        $(id("password")).setValue("123");
+        $("[value='Sign in']").click();
+        Selenide.sleep(100);
+        $(byText("There have been several failed attempts to sign in from this account or IP address. Please wait a while and try again later.")).shouldBe(Condition.visible);
+    }
+
+    @Test
+    @JiraIssue("QC5-5")
+    @AllureId("3243")
+    @ManualMember("c05-nasstasya")
+    @AutoMember("nastiykr")
+    @Component("UI")
+    @Feature("Разделы на сайте")
+    @Tags({@Tag("web")})
+    @DisplayName("Открытие вкладки features")
+    void openTabFeature() {
+        gitHubPage.openPage(BASE_URL)
+                .search(REPOSITORY)
+                .goToRepository(REPOSITORY)
+                .checkData(TAB);
+    }
+
+    @Test
+    @JiraIssue("QC5-5")
+    @AllureId("3244")
+    @ManualMember("c05-nasstasya")
+    @AutoMember("nastiykr")
+    @Component("UI")
+    @Feature("Разделы на сайте")
+    @Tags({@Tag("web")})
+    @DisplayName("Открытие страницы shop")
+    void openShopPage() {
+        gitHubPage.openPage(BASE_URL)
+                .search(REPOSITORY)
+                .goToRepository(REPOSITORY)
+                .checkData(TAB);
+    }
+}
